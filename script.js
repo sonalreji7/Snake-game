030155861f3b0fb260dbd9405edeb533413c1e80
sonalreji7 <sonalrejikl12@gmail.com>
2025-09-04 11:15:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ let dom_canvas = document.createElement("canvas");
 document.querySelector("#canvas").appendChild(dom_canvas);
 let CTX = dom_canvas.getContext("2d");
 
-// Canvas + grid setup
+// Background flash images
+let bgImages = [];
+let bgFlash = 0;
+let currentBgImage = null;
+
+// load your custom background images
+let imageSources = ["bg1.jpeg", "bg2.jpeg", "bg3.jpeg","bg4.jpeg"];
+imageSources.forEach(src => {
+  let img = new Image();
+  img.src = src;
+  bgImages.push(img);
+});
+
+// Canvas setup
 const W = (dom_canvas.width = 800);
 const H = (dom_canvas.height = 800);
-let cells = 10;
+let cells = 10; // more cells for smoother play
 let cellSize = W / cells;
+
+// Timing (FPS control)
+let lastTime = 0;
+let accumulator = 0;
+let fps = 60;           // change this to 30 for faster play
+let step = 1000 / fps;
 
 // Game state
 let snake, food, isGameOver = false, score = 0;
@@ -18,7 +37,7 @@ let requestID;
 
 // Images
 let snakeHeadImg = new Image();
-snakeHeadImg.src = "snake_head.png"; // your face image
+snakeHeadImg.src = "snake_head.png"; // face image
 let foodImg = new Image();
 foodImg.src = "apple.png"; // apple image
 
@@ -35,7 +54,7 @@ let helpers = {
   isCollision(v1, v2) { return v1.x === v2.x && v1.y === v2.y; },
   garbageCollector() { particles = particles.filter(p => p.size > 0); },
   drawGrid() {
-    CTX.lineWidth = 1.1;
+    CTX.lineWidth = 1;
     CTX.strokeStyle = "#181825";
     for (let i = 1; i < cells; i++) {
       let f = (W / cells) * i;
@@ -46,7 +65,8 @@ let helpers = {
 };
 
 // Keyboard control
-let KEY = { ArrowUp: false, ArrowRight: false, ArrowDown: false, ArrowLeft: false,
+let KEY = {
+  ArrowUp: false, ArrowRight: false, ArrowDown: false, ArrowLeft: false,
   resetState() { this.ArrowUp=this.ArrowRight=this.ArrowDown=this.ArrowLeft=false; },
   listen() {
     addEventListener("keydown", e => {
@@ -58,7 +78,6 @@ let KEY = { ArrowUp: false, ArrowRight: false, ArrowDown: false, ArrowLeft: fals
         this.resetState(); this[e.key] = true;
       }
     });
-    addEventListener("keyup", e => { if (e.key in this) this[e.key] = false; });
   }
 };
 KEY.listen();
@@ -67,9 +86,8 @@ KEY.listen();
 class Snake {
   constructor() {
     this.pos = new helpers.Vec(W/2, H/2);
-    this.dir = new helpers.Vec(0, 0);
+    this.dir = new helpers.Vec(cellSize, 0); // start moving right
     this.size = cellSize;
-    this.color = "lightgreen";
     this.history = [];
     this.total = 1;
     this.delay = 7;
@@ -86,7 +104,7 @@ class Snake {
       CTX.drawImage(snakeHeadImg, -this.size/2, -this.size/2, this.size, this.size);
       CTX.restore();
     } else {
-      CTX.fillStyle = this.color;
+      CTX.fillStyle = "lightgreen";
       CTX.fillRect(x,y,this.size,this.size);
     }
     if (this.total >= 2) {
@@ -120,12 +138,16 @@ class Snake {
     }
   }
   update() {
-    this.walls(); this.draw(); this.controlls();
+    this.walls(); this.controlls();
     if (!this.delay--) {
       if (helpers.isCollision(this.pos, food.pos)) {
         score++; dom_score.innerText = score.toString().padStart(2,"0");
         particleSplash(); food.spawn(); this.total++;
         eatSound.currentTime=0; eatSound.play();
+
+        // background image flash
+        currentBgImage = bgImages[Math.floor(Math.random() * bgImages.length)];
+        bgFlash = 1;
       }
       this.history[this.total-1] = new helpers.Vec(this.pos.x,this.pos.y);
       for (let i=0;i<this.total-1;i++) this.history[i]=this.history[i+1];
@@ -179,22 +201,14 @@ function particleSplash() {
   }
 }
 
-function clear() { CTX.clearRect(0,0,W,H); }
-
-let lastTime = 0;
-let accumulator = 0;
-let snakeSpeed = 50; // moves per second
-let step = 1000 / snakeSpeed; // ms per move
-
+// Main loop
 function loop(timestamp = 0) {
   requestID = requestAnimationFrame(loop);
-  clear();
 
   let delta = timestamp - lastTime;
   lastTime = timestamp;
   accumulator += delta;
 
-  // update snake logic only at fixed rate
   while (accumulator >= step) {
     if (!isGameOver) {
       snake.update();
@@ -205,17 +219,27 @@ function loop(timestamp = 0) {
     accumulator -= step;
   }
 
-  // always draw
+  // base background
+  CTX.fillStyle = "#000";
+  CTX.fillRect(0, 0, W, H);
+
+  // flash image fade
+  if (bgFlash > 0 && currentBgImage) {
+    CTX.save();
+    CTX.globalAlpha = bgFlash;
+    CTX.drawImage(currentBgImage, 0, 0, W, H);
+    CTX.restore();
+    bgFlash -= 0.02;
+  }
+
+  // objects
   helpers.drawGrid();
   snake.draw();
   food.draw();
 
-  for (let p of particles) {
-    p.update();
-  }
+  for (let p of particles) p.update();
   helpers.garbageCollector();
 }
-
 
 function gameOver() {
   if (score > maxScore) maxScore = score;
@@ -227,12 +251,12 @@ function gameOver() {
   CTX.fillText(`SCORE ${score}`,W/2,H/2+60);
   CTX.fillText(`MAXSCORE ${maxScore}`,W/2,H/2+80);
 }
+
+// mobile button controls
 function pressKey(direction) {
   KEY.resetState();
   KEY[direction] = true;
 }
-
-// assign to buttons
 document.querySelector("#U").addEventListener("click", () => pressKey("ArrowUp"));
 document.querySelector("#D").addEventListener("click", () => pressKey("ArrowDown"));
 document.querySelector("#L").addEventListener("click", () => pressKey("ArrowLeft"));
